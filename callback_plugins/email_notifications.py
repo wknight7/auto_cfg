6bc8199144435dfcdc7b2876b0ac7b29b13a4efb
Bill Knight <bill@gmail.com>
2024-12-12 11:31:23 -0500

    added email notifications

--- callback_plugins/email_notifications.py
+++ callback_plugins/email_notifications.py
@@ -14,7 +14,6 @@ class CallbackModule(CallbackBase):
         vault_password = os.getenv('VAULT_PASS')
         if vault_password is None:
             raise ValueError("VAULT_PASS environment variable not set")
-        print(f"Vault password: {vault_password}")  # Debugging line
         self.loader = DataLoader()
         self.vault = VaultLib([(None, VaultSecret(vault_password.encode('utf-8')))])
 
@@ -37,12 +36,22 @@ class CallbackModule(CallbackBase):
         return any(group in groups for group in workstation_groups)
 
     def send_email(self, subject, body):
-        to_email = "your_email@example.com"
-        app_password = self.get_vault_var('outlook_app_password')
+        to_email = "billknig@gmail.com"  # Your email address
+        app_password = self.get_vault_var('gmail_app_password')
         print(f"Retrieved app password: '{app_password}'")  # Debugging line
+
+        # Email headers
+        headers = f"From: Hal @ Ansible <billknig@gmail.com>\n"
+        headers += f"To: {to_email}\n"
+        headers += f"Subject: {subject}\n\n"
+
+        # Combine headers and body
+        email_message = headers + body
+
         with open('/tmp/email.txt', 'w') as email_file:
-            email_file.write(body)
-        os.system(f'cat /tmp/email.txt | msmtp --host=smtp-mail.outlook.com --port=587 --user=billknig@outlook.com --auth=on --passwordeval="echo {app_password}" --tls --tls-certcheck=off --from=billknig@outlook.com -t {to_email}')
+            email_file.write(email_message)
+        
+        os.system(f'cat /tmp/email.txt | msmtp --host=smtp.gmail.com --port=587 --user=billknig@gmail.com --auth=on --passwordeval="echo {app_password}" --tls --tls-certcheck=off --from=billknig@gmail.com -t {to_email}')
 
     def get_vault_var(self, var_name):
         vault_file_path = 'group_vars/vault.yml'
@@ -50,12 +59,4 @@ class CallbackModule(CallbackBase):
             vault_content = vault_file.read()
             decrypted_content = self.vault.decrypt(vault_content)
             vault_data = yaml.safe_load(decrypted_content)  # Load the decrypted content as YAML
-            print(f"Decrypted vault data: {vault_data}")  # Debugging line
         return vault_data.get(var_name)
-
-
-
-
-
-
-
